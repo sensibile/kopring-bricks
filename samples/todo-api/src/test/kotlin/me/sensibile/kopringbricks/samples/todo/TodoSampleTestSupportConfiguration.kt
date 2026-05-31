7dd460b6debef47922a9d7c4825b449aec197cdc
Wonjay Jung <82997779+sensibile@users.noreply.github.com>
2026-05-31 22:33:14 +0900

    Improve starter template tests and sample test setup (#22)

---
+++ samples/todo-api/src/test/kotlin/me/sensibile/kopringbricks/samples/todo/TodoSampleTestSupportConfiguration.kt
@@ -0,0 +1,23 @@
+package me.sensibile.kopringbricks.samples.todo
+
+import me.sensibile.kopringbricks.testsupport.audit.RecordingAuditEventPublisher
+import me.sensibile.kopringbricks.testsupport.outbox.InMemoryOutboxEventRepository
+import me.sensibile.kopringbricks.testsupport.outbox.RecordingOutboxEventPublisher
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+
+@TestConfiguration
+class TodoSampleTestSupportConfiguration {
+    @Bean
+    @Primary
+    fun recordingAuditEventPublisher(): RecordingAuditEventPublisher = RecordingAuditEventPublisher()
+
+    @Bean
+    @Primary
+    fun inMemoryOutboxEventRepository(): InMemoryOutboxEventRepository = InMemoryOutboxEventRepository()
+
+    @Bean
+    @Primary
+    fun recordingOutboxEventPublisher(): RecordingOutboxEventPublisher = RecordingOutboxEventPublisher()
+}
