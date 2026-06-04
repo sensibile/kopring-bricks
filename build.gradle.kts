403abba71a5cf70b54cc9a21f7d4ae937c8fcadd
sensibile <wjjung@g.skku.edu>
2026-06-04 21:47:41 +0900

    Add Kover coverage reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kover) apply false
     alias(libs.plugins.spring.boot) apply false
 }
 
--- samples/todo-api/build.gradle.kts
+++ samples/todo-api/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kover)
     alias(libs.plugins.spring.boot)
 }
 
